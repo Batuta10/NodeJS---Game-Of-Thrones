07f50308d7d13cf2f2dac27e7cd96c34c4f5aa25
Batuta10 <batuta2604@gmail.com>
2017-10-05 09:27:13 -0300

    Implement validator on cadastro

--- app/controllers/cadastroControllers.js
+++ app/controllers/cadastroControllers.js
@@ -1,3 +1,20 @@
 module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro');
+    res.render('cadastro', {validacao: {}, dadosForm: {}} );
+}
+
+module.exports.cadastrar = function (application, req, res) {
+    var dadosForm = req.body;
+    
+        req.assert('nome', 'Nome é obrigatorio não pode passar de 45 ou ser menor que 3 letras').notEmpty();
+        req.assert('usuario', 'Usuario é obrigatorio não pode passar de 45 ou ser menor que 3 letras').notEmpty();
+        req.assert('senha', 'Senha é obrigatorio não pode passar de 45 ou ser menor que 8 letras').notEmpty();
+        req.assert('casas', 'Casa não pode estar vazio').notEmpty();
+    
+        var erros = req.validationErrors();
+    
+        if (erros) {
+            res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+            return;
+        }
+        res.send('podemos cadastrar');
 }
